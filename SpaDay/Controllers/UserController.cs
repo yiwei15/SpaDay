6e154debbb224515ce3179c2d60c8f41f14e79e0
Yiwei Zhao <yiweizhao15@gmail.com>
2020-07-17 11:02:29 -0500

    Ch14 Moedels and model binding studio and bonus solution added

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match!";
+                ViewBag.userName = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Add");
+            }
+        }
+
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email  { get; set; }
+        public string Password { get; set; }
+        public DateTime JoinDate { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(string name, string email, string password):this()
+        {
+            Username = name;
+            Email = email;
+            Password = password;
+            JoinDate = DateTime.Now;
+        }
+    }
+}
